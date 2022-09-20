fb983e869c8ee877557a79a9029cf4e5f9398e69
Alex de Andrade Soares <49697325+alexandrades@users.noreply.github.com>
2022-09-20 13:34:32 -0300

    Correção da última aula

--- src/edu/ufsj/lox/Scanner.java
+++ src/edu/ufsj/lox/Scanner.java
@@ -44,60 +44,16 @@ class Scanner {
 				String text = source.substring(start, current);
 				tokens.add(new Token(type, text, literal, line));
 		}
-
+		
 		private boolean match(char expected) {
-			if(isAtEnd()) return false;
-			if(source.charAt(current) != expected) return false;
-
+			if (isAtEnd()) return false;
+			if (source.charAt(current) != expected)
+					return false;
+			
 			current++;
 			return true;
 		}
 		
-		private char peek() {
-			if(isAtEnd()) return '\0';
-			return source.charAt(current);
-		}
-
-		private void string() {
-			
-			while(peek() != '"' && !isAtEnd()){
-				if(peek() == '\n') line++;
-				advance();
-			} 
-			// abre " sem o perspectivo fecha "
-			if(isAtEnd()) {
-				Lox.error(line, "Unterminated string.");
-				return;
-			}
-			// fecha "
-			advance();
-			String val = source.substring(start + 1, current - 1);
-			addToken(STRING, val);
-		}
-
-		private boolean isDigit(char c) {
-			return c >= '0' && c <= '9';
-		}
-
-		private void number() {
-			while(isDigit(peek())) advance();
-			// Procura parte fracionária
-			if(peek() == '.' && isDigit(peekNext())) {
-				//consome o "."
-				advance();
-				while(isDigit(peek())) advance()
-			}
-			addToken(NUMBER, Double.parseDouble(source.substring(start + 1, current - 1)));
-		}
-
-		private char peekNext() {
-			if(current + 1 >= source.length())
-				return '\0';
-		
-			return source.charAt(current + 1);
-
-		}
-
 		private void scanToken() {
 				char c = advance();
 				switch(c) {
@@ -111,138 +67,91 @@ class Scanner {
 						case '+': addToken(PLUS); break;
 						case ';': addToken(SEMICOLON); break;
 						case '*': addToken(STAR); break;
-						case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
-						case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-						case '<': addT                                                start = current;
-24
-                                                scanToken();
-25
-                                }
-26
-                                tokens.add(new Token(EOF, "", null, line));
-27
-                                return tokens;
-28
-                }
-29
-                
-30
-                private boolean isAtEnd() {
-31
-                                return current >= source.length();
-32
-                }
-33
-                
-34
-                private char advance() {
-35
-                                current++;
-36
-                                return source.charAt(current - 1);
-37
-                }
-38
-                
-39
-                private void addToken(TokenType type) {
-40
-                                addToken(type, null);
-41
-                }
-42
-                
-43
-                private void addToken(TokenType type, Object literal) {
-44
-                                String text = source.substring(start, current);
-45
-                                tokens.add(new Token(type, text, literal, line));
-46
-                }
-47
-​
-48
-                private boolean match(char expected) {
-49
-                        if(isAtEnd()) return false;
-50
-                        if(source.charAt(current) != expected) return false;
-51
-​
-52
-                        current++;
-53
-                        return true;
-54
-                }
-55
-                
-56
-                private char peek() {
-57
-                        if(isAtEnd()) return '\0';
-58
-                        return source.charAt(current);
-59
-                }
-60
-​
-61
-                private void string() {
-62
-                        
-63
-                        while(peek() != '"' && !isAtEnd()){
-64
-                                if(peek() == '\n') line++;
-65
-                                advance();
-66
-                        } 
-67
-                        // abre " sem o perspectivo fecha "
-68
-                        if(isAtEnd()) {
-69
-                                Lox.error(line, "Unterminated string.");
-70
-                                return ''
-71
-                        }
-72
-                        // fecha "
-73
-                        advance();
-74
-                        String val = source.substring(start + 1, current - 1);
-75
-                        addToken(STRING, val);
-76
-                }
-77
-​
-78
-                private boolean isDigit(char c) {oken(match('=') ? LESS_EQUAL : LESS); break;
-						case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+						case '!':
+								  addToken(match('=') ? BANG_EQUAL : BANG);
+								  break;
+						case '=': 
+								  addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+								  break;
+						case '<': 
+								  addToken(match('=') ? LESS_EQUAL : LESS);
+								  break;
+						case '>':
+								  addToken(match('=') ? GREATER_EQUAL : GREATER);
+								  break;
 						case ' ':
 						case '\r':
-						case '\t': break;
-						case '"': string(); break;
+						case '\t':
+								  break;
+						case '\n':
+								  line++;
+								  break;
 						case '/':
-							if(match('/')) {
-								while(peek() != '\n' && !isAtEnd()) advance();
-							} else {
-								addToken(SLASH);
-							}
-							break;
-						default:
-							if(isDigit()){
-								number();
-							} else {
-								Lox.error(line,  "Unexpected character.");
-							}
-							break;
+								  if (match('/')) {
+										// comentarios de uma linha
+										while (peek() != '\n' && !isAtEnd())
+												advance();
+								  } else {
+										addToken(SLASH);
+								  }
+								  break;
+						case '"': string(); break;
+						default:  
+								  if (isDigit(c)) {
+									  	  number();
+								  } else {
+									  	  Lox.error(line, "Unexpected character.");
+								  }
+								  break;
 				}
 		}
+		
+		private void number() {
+				while (isDigit(peek()))
+								advance();
+				// procura a parte fracionaria
+				if (peek() == '.' && isDigit(peekNext())) {
+						// consome o "."
+						advance();
+						
+						while (isDigit(peek()))
+								advance();
+				}
+				addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+		}
+		
+		private char peekNext() {
+				if (current + 1 >= source.length())
+							return '\0';
+				
+				return source.charAt(current + 1);
+		}
+		
+		private boolean isDigit(char c) {
+				return c >= '0' && c <= '9';
+		}
+		
+		private char peek() {
+			if (isAtEnd()) return '\0';
+			return source.charAt(current);
+		}
+		
+		private void string() {
+				while (peek() != '"' && !isAtEnd()) {
+						if (peek() == '\n')
+								line++;
+						advance();
+				}
+				// abre " sem o respectivo fecha "
+				if (isAtEnd()) {
+						Lox.error(line, "Unterminated string.");
+						return;
+				}
+				// achei fecha "
+				advance();
+				// remove os "s
+				String val = source.substring(start + 1, current - 1);
+				addToken(STRING, val);
+		}	
+		
 }
